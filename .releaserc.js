b87760d4e66948a83e98c5fb269c98fe69748142
minkyEazel <minky@eazel.net>
2023-08-01 22:45:36 +0900

    💚 (ci/cd): Add ci/cd config

---
+++ .releaserc.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const { promisify } = require('util')
+const dateFormat = require('dateformat')
+const readFileAsync = promisify(require('fs').readFile)
+
+// Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
+const TEMPLATE_DIR = path.join(
+  __dirname,
+  'node_modules',
+  'semantic-release-gitmoji',
+  'lib',
+  'assets',
+  'templates'
+)
+
+// the *.hbs template and partials should be passed as strings of contents
+const template = readFileAsync(path.join(TEMPLATE_DIR, 'default-template.hbs'))
+const commitTemplate = readFileAsync(path.join(TEMPLATE_DIR, 'commit-template.hbs'))
+
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/github',
+    '@semantic-release/npm',
+    [
+      'semantic-release-gitmoji',
+      {
+        releaseRules: {
+          major: [':boom:'],
+          minor: [':sparkles:'],
+          patch: [
+            ':bug:',
+            ':ambulance:',
+            ':lock:',
+            ':arrow_up:',
+            ':art:',
+            ':zap:',
+            ':iphone:',
+            ':lipstick:',
+            ':green_heart:',
+          ],
+        },
+        releaseNotes: {
+          template,
+          partials: { commitTemplate },
+          helpers: {
+            datetime: function (format = 'UTC:yyyy-mm-dd') {
+              return dateFormat(new Date(), format)
+            },
+          },
+          issueResolution: {
+            template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
+            baseUrl: 'https://github.com',
+            source: 'github.com',
+            removeFromCommit: false,
+            regex: /#\d+/g,
+          },
+        },
+      },
+    ],
+  ],
+}
