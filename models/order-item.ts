54a5ebf417c7dba78c722bbe03e4c31742f1d219
minkyEazel <minky@eazel.net>
2023-08-01 22:30:06 +0900

    💡 (comment): Delete comment

--- models/order-item.ts
+++ models/order-item.ts
@@ -1,17 +1,3 @@
-// const Sequelize = require('sequelize');
-
-// const sequelize = require('../util/database');
-
-// const OrderItem = sequelize.define('orderItem', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   quantity: Sequelize.INTEGER
-// });
-
 class OrderItem {
   id: number
   constructor(id: number) {
